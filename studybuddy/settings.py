f151927da11d4c91c7211f832f179716130d72b0
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-06-22 14:26:21 +0545

    Update settings.py

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / '/images/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
